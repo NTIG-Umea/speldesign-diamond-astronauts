cb26a8df6cee8f76fab986f2a8ebc22c7622d828
elloot <elliot.duchek@gmail.com>
2020-12-01 11:12:23 +0100

    remove redundant comment

--- parcel-port/src/PlayScene.js
+++ parcel-port/src/PlayScene.js
@@ -16,81 +16,8 @@ export default class PlayScene extends Phaser.Scene {
   create() {
     this.mazeGraphics = this.add.graphics();
     this.maze = mazeGenerator();
-    // var moves = [];
-    // this.maze = [];
-    // for (var i = 0; i < gameOptions.mazeHeight; i++) {
-    //   this.maze[i] = [];
-    //   for (var j = 0; j < gameOptions.mazeWidth; j++) {
-    //     this.maze[i][j] = 1;
-    //   }
-    // }
-    // console.log(this.maze);
-    // var posY = gameOptions.mazeStartingX;
-    // var posX = gameOptions.mazeStartingY;
-    // this.maze[posY][posX] = 0;
-    // moves.push(posX + posX * gameOptions.mazeWidth);
-    // while (moves.length) {
-    //   var possibleDirections = '';
-    //   if (
-    //     posY + 2 > 0 &&
-    //     posY + 2 < gameOptions.mazeHeight - 1 &&
-    //     this.maze[posY + 2][posX] == 1
-    //   ) {
-    //     possibleDirections += 'S';
-    //   }
-    //   if (
-    //     posY - 2 > 0 &&
-    //     posY - 2 < gameOptions.mazeHeight - 1 &&
-    //     this.maze[posY - 2][posX] == 1
-    //   ) {
-    //     possibleDirections += 'N';
-    //   }
-    //   if (
-    //     posX - 2 > 0 &&
-    //     posX - 2 < gameOptions.mazeWidth - 1 &&
-    //     this.maze[posY][posX - 2] == 1
-    //   ) {
-    //     possibleDirections += 'W';
-    //   }
-    //   if (
-    //     posX + 2 > 0 &&
-    //     posX + 2 < gameOptions.mazeWidth - 1 &&
-    //     this.maze[posY][posX + 2] == 1
-    //   ) {
-    //     possibleDirections += 'E';
-    //   }
-    //   if (possibleDirections) {
-    //     var move = Phaser.Math.Between(0, possibleDirections.length - 1);
-    //     switch (possibleDirections[move]) {
-    //       case 'N':
-    //         this.maze[posY - 2][posX] = 0;
-    //         this.maze[posY - 1][posX] = 0;
-    //         posY -= 2;
-    //         break;
-    //       case 'S':
-    //         this.maze[posY + 2][posX] = 0;
-    //         this.maze[posY + 1][posX] = 0;
-    //         posY += 2;
-    //         break;
-    //       case 'W':
-    //         this.maze[posY][posX - 2] = 0;
-    //         this.maze[posY][posX - 1] = 0;
-    //         posX -= 2;
-    //         break;
-    //       case 'E':
-    //         this.maze[posY][posX + 2] = 0;
-    //         this.maze[posY][posX + 1] = 0;
-    //         posX += 2;
-    //         break;
-    //     }
-    //     moves.push(posX + posY * gameOptions.mazeWidth);
-    //   } else {
-    //     var back = moves.pop();
-    //     posY = Math.floor(back / gameOptions.mazeWidth);
-    //     posX = back % gameOptions.mazeWidth;
-    //   }
-    // }
     this.mazeGraphics = drawMaze();
+
     var easystar = new EasyStar.js();
     easystar.setGrid(this.maze);
     easystar.setAcceptableTiles([0]);
@@ -180,21 +107,6 @@ export default class PlayScene extends Phaser.Scene {
     }
   }
 
-  // drawMaze(posX, posY) {
-  //   this.mazeGraphics.fillStyle(0x000000);
-  //   for (var i = 0; i < gameOptions.mazeHeight; i++) {
-  //     for (var j = 0; j < gameOptions.mazeWidth; j++) {
-  //       if (this.maze[i][j] == 1) {
-  //         this.mazeGraphics.fillRect(
-  //           j * gameOptions.tileSize,
-  //           i * gameOptions.tileSize,
-  //           gameOptions.tileSize,
-  //           gameOptions.tileSize
-  //         );
-  //       }
-  //     }
-  //   }
-  // }
   drawPath(path) {
     var i = 0;
     this.time.addEvent({
