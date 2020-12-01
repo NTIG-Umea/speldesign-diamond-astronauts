eccf29d060e36e05f8f871a323e71708b759f0f2
milkyas23 <milkyaseyob.habte@elev.ga.ntig.se>
2020-12-01 10:48:47 +0100

    move gameOptions to another file

--- src/index.js
+++ src/index.js
@@ -6,17 +6,17 @@ var game;
 var player;
 var score = 0;
 
-var gameOptions = {
-  mazeWidth: 31,
-  mazeHeight: 31,
-  tileSize: 64,
-  playerStartingX: 1,
-  playerStartingY: 1,
-  mazeStartingX: 1,
-  mazeStartingY: 1,
-  mazeEndX: 31 - 2, // this should read the value from gameOptions.mazeWidth
-  mazeEndY: 31 - 2,
-};
+// var gameOptions = {
+//   mazeWidth: 31,
+//   mazeHeight: 31,
+//   tileSize: 64,
+//   playerStartingX: 1,
+//   playerStartingY: 1,
+//   mazeStartingX: 1,
+//   mazeStartingY: 1,
+//   mazeEndX: 31 - 2, // this should read the value from gameOptions.mazeWidth
+//   mazeEndY: 31 - 2,
+// };
 
 window.onload = function () {
   var gameConfig = {
