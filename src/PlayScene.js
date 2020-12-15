97bc0b88c24923629fea46b7ea1d1a606bd8af70
elloot <elliot.duchek@gmail.com>
2020-12-15 10:46:53 +0100

    add incrementation for maze height and width when player wins, also recalculate maze end location when new maze is to be generated

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -88,6 +88,10 @@ export default class PlayScene extends Phaser.Scene {
     this.score++;
     alert(`Your score was: ${this.score}`); // should use some Phaser implementation of this
     console.log(this);
+    gameOptions.mazeWidth += gameOptions.mazeSizeIncrement;
+    gameOptions.mazeHeight += gameOptions.mazeSizeIncrement;
+    gameOptions.mazeEndX = gameOptions.mazeWidth - 2;
+    gameOptions.mazeEndY = gameOptions.mazeHeight - 2;
     this.scene.start('play');
   }
 
@@ -128,7 +132,7 @@ export default class PlayScene extends Phaser.Scene {
     let worldView = this.cameras.main.worldView;
     this.playerHB.setPosition(worldView.x, worldView.y);
     // decrease player health as game goes on
-    this.playerHB.decrease(0.01);
+    this.playerHB.decrease(gameOptions.damagePerUpdate);
     if (this.playerHB.value <= 0) {
       alert(`Your score was: ${this.score}`);
       this.scene.switch('end');
